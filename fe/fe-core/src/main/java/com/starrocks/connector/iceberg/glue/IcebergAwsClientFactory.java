deep-bi/starrocks

commit 7bffe4b8290f0d20d69bff358e7b654164fc842d

[Refactor] Refactor iceberg catalog (#23618)
This pr is mainly to process #21200.
Major modification:

use open source iceberg HiveCatalog and GlueCatalog and RESTCatalog instead of override class
implement create/drop db/table related interface of glue catalog and rest catalog.
refactor IcebergCatalog interface
refactor IcebergCachingFileIO
refactor MetricsReporter
use createTable instead of newTableTxn
implement IcebergAwsClientFactory to support glue catalog.

Signed-off-by: stephen <stephen5217@163.com>

fe/fe-core/src/main/java/com/starrocks/connector/iceberg/glue/IcebergAwsClientFactory.java
@@ -1,0 +1,208 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.connector.iceberg.glue;
+
+import com.google.common.base.Preconditions;
+import org.apache.iceberg.aws.AwsClientFactory;
+import org.apache.iceberg.aws.AwsProperties;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.glue.GlueClient;
+import software.amazon.awssdk.services.glue.GlueClientBuilder;
+import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_ACCESS_KEY;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_ENDPOINT;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_EXTERNAL_ID;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_IAM_ROLE_ARN;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_REGION;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_SECRET_KEY;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_USE_AWS_SDK_DEFAULT_BEHAVIOR;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_GLUE_USE_INSTANCE_PROFILE;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_ACCESS_KEY;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_ENDPOINT;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_EXTERNAL_ID;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_IAM_ROLE_ARN;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_REGION;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_SECRET_KEY;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_USE_AWS_SDK_DEFAULT_BEHAVIOR;
+import static com.starrocks.credential.CloudConfigurationConstants.AWS_S3_USE_INSTANCE_PROFILE;
+
+public class IcebergAwsClientFactory implements AwsClientFactory {
+    public IcebergAwsClientFactory() {
+    }
+
+    private AwsProperties awsProperties;
+
+    private boolean s3UseAWSSDKDefaultBehavior;
+    private boolean s3UseInstanceProfile;
+    private String s3AccessKey;
+    private String s3SecretKey;
+    private String s3IamRoleArn;
+    private String s3ExternalId;
+    private String s3Region;
+    private String s3Endpoint;
+
+    private boolean glueUseAWSSDKDefaultBehavior;
+    private boolean glueUseInstanceProfile;
+    private String glueAccessKey;
+    private String glueSecretKey;
+    private String glueIamRoleArn;
+    private String glueExternalId;
+    private String glueRegion;
+    private String glueEndpoint;
+
+
+    @Override
+    public void initialize(Map<String, String> properties) {
+        this.awsProperties = new AwsProperties(properties);
+
+        s3UseAWSSDKDefaultBehavior = Boolean.parseBoolean(properties.getOrDefault(AWS_S3_USE_AWS_SDK_DEFAULT_BEHAVIOR, "false"));
+        s3UseInstanceProfile = Boolean.parseBoolean(properties.getOrDefault(AWS_S3_USE_INSTANCE_PROFILE, "false"));
+        s3AccessKey = properties.getOrDefault(AWS_S3_ACCESS_KEY, "");
+        s3SecretKey = properties.getOrDefault(AWS_S3_SECRET_KEY, "");
+        s3IamRoleArn = properties.getOrDefault(AWS_S3_IAM_ROLE_ARN, "");
+        s3ExternalId = properties.getOrDefault(AWS_S3_EXTERNAL_ID, "");
+        s3Region = properties.getOrDefault(AWS_S3_REGION, "");
+        s3Endpoint = properties.getOrDefault(AWS_S3_ENDPOINT, "");
+
+        glueUseAWSSDKDefaultBehavior = Boolean.parseBoolean(
+                properties.getOrDefault(AWS_GLUE_USE_AWS_SDK_DEFAULT_BEHAVIOR, "false"));
+        glueUseInstanceProfile = Boolean.parseBoolean(properties.getOrDefault(AWS_GLUE_USE_INSTANCE_PROFILE, "false"));
+        glueAccessKey = properties.getOrDefault(AWS_GLUE_ACCESS_KEY, "");
+        glueSecretKey = properties.getOrDefault(AWS_GLUE_SECRET_KEY, "");
+        glueIamRoleArn = properties.getOrDefault(AWS_GLUE_IAM_ROLE_ARN, "");
+        glueExternalId = properties.getOrDefault(AWS_GLUE_EXTERNAL_ID, "");
+        glueRegion = properties.getOrDefault(AWS_GLUE_REGION, "");
+        glueEndpoint = properties.getOrDefault(AWS_GLUE_ENDPOINT, "");
+    }
+
+    @Override
+    public S3Client s3() {
+        if (s3UseAWSSDKDefaultBehavior) {
+            AwsCredentialsProvider provider = DefaultCredentialsProvider.builder().build();
+            return S3Client.builder().credentialsProvider(provider).build();
+        }
+
+        AwsCredentialsProvider baseAWSCredentialsProvider =
+                getBaseAWSCredentialsProvider(s3UseInstanceProfile, s3AccessKey, s3SecretKey);
+        S3ClientBuilder s3ClientBuilder = S3Client.builder();
+        if (!s3IamRoleArn.isEmpty()) {
+            StsClient stsClient = StsClient.builder().credentialsProvider(baseAWSCredentialsProvider).build();
+            String sessionName = UUID.randomUUID().toString();
+            AssumeRoleRequest.Builder assumeRoleBuilder = AssumeRoleRequest.builder();
+            assumeRoleBuilder.roleArn(s3IamRoleArn);
+            if (!s3ExternalId.isEmpty()) {
+                assumeRoleBuilder.externalId(s3ExternalId);
+            }
+            assumeRoleBuilder.roleSessionName(sessionName);
+            StsAssumeRoleCredentialsProvider.Builder builder = StsAssumeRoleCredentialsProvider.builder()
+                    .stsClient(stsClient)
+                    .refreshRequest(AssumeRoleRequest.builder()
+                            .build());
+            s3ClientBuilder.credentialsProvider(builder.build());
+        } else {
+            s3ClientBuilder.credentialsProvider(baseAWSCredentialsProvider);
+        }
+
+        if (!s3Region.isEmpty()) {
+            s3ClientBuilder.region(Region.of(s3Region));
+        }
+
+        if (!s3Endpoint.isEmpty()) {
+            s3ClientBuilder.endpointOverride(URI.create(s3Endpoint));
+        }
+
+        s3ClientBuilder.applyMutation(awsProperties::applyHttpClientConfigurations);
+
+        return s3ClientBuilder.build();
+    }
+
+    @Override
+    public GlueClient glue() {
+        if (glueUseAWSSDKDefaultBehavior) {
+            AwsCredentialsProvider provider = DefaultCredentialsProvider.builder().build();
+            return GlueClient.builder().credentialsProvider(provider).build();
+        }
+
+        AwsCredentialsProvider baseAWSCredentialsProvider =
+                getBaseAWSCredentialsProvider(glueUseInstanceProfile, glueAccessKey, glueSecretKey);
+        GlueClientBuilder glueClientBuilder = GlueClient.builder();
+        if (!glueIamRoleArn.isEmpty()) {
+            StsClient stsClient = StsClient.builder().credentialsProvider(baseAWSCredentialsProvider).build();
+            String sessionName = UUID.randomUUID().toString();
+            AssumeRoleRequest.Builder assumeRoleBuilder = AssumeRoleRequest.builder();
+            assumeRoleBuilder.roleArn(glueIamRoleArn);
+            if (!glueExternalId.isEmpty()) {
+                assumeRoleBuilder.externalId(glueExternalId);
+            }
+            assumeRoleBuilder.roleSessionName(sessionName);
+            StsAssumeRoleCredentialsProvider.Builder builder =
+                    StsAssumeRoleCredentialsProvider.builder()
+                            .stsClient(stsClient)
+                            .refreshRequest(AssumeRoleRequest.builder().build());
+            glueClientBuilder.credentialsProvider(builder.build());
+        } else {
+            glueClientBuilder.credentialsProvider(baseAWSCredentialsProvider);
+        }
+
+        if (!glueRegion.isEmpty()) {
+            glueClientBuilder.region(Region.of(glueRegion));
+        }
+
+        if (!glueEndpoint.isEmpty()) {
+            glueClientBuilder.endpointOverride(URI.create(glueEndpoint));
+        }
+
+        glueClientBuilder.applyMutation(awsProperties::applyHttpClientConfigurations);
+        return glueClientBuilder.build();
+    }
+
+    private AwsCredentialsProvider getBaseAWSCredentialsProvider(boolean useInstanceProfile, String accessKey, String secretKey) {
+        if (useInstanceProfile) {
+            return InstanceProfileCredentialsProvider.builder().build();
+        } else if (!accessKey.isEmpty() && !secretKey.isEmpty()) {
+            return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        } else {
+            Preconditions.checkArgument(false, "Unreachable");
+            return AnonymousCredentialsProvider.create();
+        }
+    }
+
+    @Override
+    public KmsClient kms() {
+        return null;
+    }
+
+    @Override
+    public DynamoDbClient dynamo() {
+        return null;
+    }
+}

fe/fe-core/pom.xml
@@ -287,21 +287,24 @@ under the License.
             <groupId>org.apache.iceberg</groupId>
             <artifactId>iceberg-aws</artifactId>
             <version>${iceberg.version}</version>
-            <scope>runtime</scope>
         </dependency>
 
         <dependency>
             <groupId>software.amazon.awssdk</groupId>
             <artifactId>bundle</artifactId>
             <version>${awsv2.version}</version>
-            <scope>runtime</scope>
         </dependency>
 
         <dependency>
             <groupId>software.amazon.awssdk</groupId>
             <artifactId>url-connection-client</artifactId>
             <version>${awsv2.version}</version>
-            <scope>runtime</scope>
+            <exclusions>
+                <exclusion>
+                    <artifactId>utils</artifactId>
+                    <groupId>software.amazon.awssdk</groupId>
+                </exclusion>
+            </exclusions>
         </dependency>
 
         <dependency>

fe/fe-core/src/main/java/com/starrocks/catalog/IcebergTable.java
@@ -29,7 +29,6 @@ import com.starrocks.common.io.Text;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.connector.iceberg.IcebergApiConverter;
 import com.starrocks.connector.iceberg.IcebergCatalogType;
-import com.starrocks.connector.iceberg.cost.IcebergMetricsReporter;
 import com.starrocks.server.CatalogMgr;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.thrift.TColumn;
@@ -49,7 +48,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.starrocks.connector.iceberg.IcebergConnector.ICEBERG_CATALOG_TYPE;
@@ -68,7 +66,6 @@ public class IcebergTable extends Table {
     private String remoteDbName;
     private String remoteTableName;
     private String resourceName;
-    private Optional<IcebergMetricsReporter> metricsReporter = Optional.empty();
 
     private Map<String, String> icebergProperties = Maps.newHashMap();
     private List<Column> partitionColumns;
@@ -80,13 +77,6 @@ public class IcebergTable extends Table {
     public IcebergTable(long id, String srTableName, String catalogName, String resourceName, String remoteDbName,
                         String remoteTableName, List<Column> schema, org.apache.iceberg.Table nativeTable,
                         Map<String, String> icebergProperties) {
-        this(id, srTableName, catalogName, resourceName, remoteDbName, remoteTableName, schema, nativeTable,
-                icebergProperties, Optional.empty());
-    }
-
-    public IcebergTable(long id, String srTableName, String catalogName, String resourceName, String remoteDbName,
-                        String remoteTableName, List<Column> schema, org.apache.iceberg.Table nativeTable,
-                        Map<String, String> icebergProperties, Optional<IcebergMetricsReporter> metricsReporter) {
         super(id, srTableName, TableType.ICEBERG, schema);
         this.catalogName = catalogName;
         this.resourceName = resourceName;
@@ -94,7 +84,6 @@ public class IcebergTable extends Table {
         this.remoteTableName = remoteTableName;
         this.nativeTable = nativeTable;
         this.icebergProperties = icebergProperties;
-        this.metricsReporter = metricsReporter;
     }
 
     public String getCatalogName() {
@@ -164,10 +153,6 @@ public class IcebergTable extends Table {
     public List<String> getPartitionColumnNames() {
         return getPartitionColumns().stream().filter(java.util.Objects::nonNull).map(Column::getName)
                 .collect(Collectors.toList());
-    }
-
-    public Optional<IcebergMetricsReporter.IcebergScanReportWithCounter> reportScanMetrics() {
-        return metricsReporter.map(IcebergMetricsReporter::lastReport);
     }
 
     @Override
@@ -299,7 +284,6 @@ public class IcebergTable extends Table {
         private List<Column> fullSchema;
         private Map<String, String> icebergProperties;
         private org.apache.iceberg.Table nativeTable;
-        private Optional<IcebergMetricsReporter> metricsReporter = Optional.empty();
 
         public Builder() {
         }
@@ -349,14 +333,9 @@ public class IcebergTable extends Table {
             return this;
         }
 
-        public Builder setMetricsReporter(Optional<IcebergMetricsReporter> metricsReporter) {
-            this.metricsReporter = metricsReporter;
-            return this;
-        }
-
         public IcebergTable build() {
             return new IcebergTable(id, srTableName, catalogName, resourceName, remoteDbName, remoteTableName,
-                    fullSchema, nativeTable, icebergProperties, metricsReporter);
+                    fullSchema, nativeTable, icebergProperties);
         }
     }
 }

fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergApiConverter.java
@@ -26,7 +26,6 @@ import com.starrocks.catalog.StructType;
 import com.starrocks.catalog.Type;
 import com.starrocks.connector.exception.StarRocksConnectorException;
 import com.starrocks.connector.hive.RemoteFileInputFormat;
-import com.starrocks.connector.iceberg.cost.IcebergMetricsReporter;
 import com.starrocks.thrift.TIcebergColumnStats;
 import com.starrocks.thrift.TIcebergDataFile;
 import com.starrocks.thrift.TIcebergSchema;
@@ -48,14 +47,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.starrocks.connector.ColumnTypeConverter.fromIcebergType;
 import static com.starrocks.connector.ConnectorTableId.CONNECTOR_ID_GENERATOR;
 import static com.starrocks.connector.iceberg.IcebergConnector.ICEBERG_CATALOG_TYPE;
 import static com.starrocks.server.CatalogMgr.ResourceMappingCatalog.toResourceName;
-import static org.apache.iceberg.hive.IcebergHiveCatalog.LOCATION_PROPERTY;
 
 public class IcebergApiConverter {
     private static final Logger LOG = LogManager.getLogger(IcebergApiConverter.class);
@@ -63,8 +60,7 @@ public class IcebergApiConverter {
     private static final int FAKE_FIELD_ID = -1;
 
     public static IcebergTable toIcebergTable(Table nativeTbl, String catalogName, String remoteDbName,
-                                              String remoteTableName, String nativeCatalogType,
-                                              Optional<IcebergMetricsReporter> metricsReporter) {
+                                              String remoteTableName, String nativeCatalogType) {
         IcebergTable.Builder tableBuilder = IcebergTable.builder()
                 .setId(CONNECTOR_ID_GENERATOR.getNextId().asInt())
                 .setSrTableName(remoteTableName)
@@ -74,8 +70,7 @@ public class IcebergApiConverter {
                 .setRemoteTableName(remoteTableName)
                 .setNativeTable(nativeTbl)
                 .setFullSchema(toFullSchemas(nativeTbl))
-                .setIcebergProperties(toIcebergProps(nativeCatalogType))
-                .setMetricsReporter(metricsReporter);
+                .setIcebergProperties(toIcebergProps(nativeCatalogType));
 
         return tableBuilder.build();
     }
@@ -103,10 +98,6 @@ public class IcebergApiConverter {
             builder.identity(field);
         }
         return builder.build();
-    }
-
-    public static Optional<String> getTableLocation(Map<String, String> tableProperties) {
-        return Optional.ofNullable(tableProperties.get(LOCATION_PROPERTY));
     }
 
     public static org.apache.iceberg.types.Type toIcebergColumnType(Type type) {

fe/fe-core/src/main/java/com/starrocks/connector/iceberg/IcebergCatalog.java
@@ -16,36 +16,18 @@
 package com.starrocks.connector.iceberg;
 
 import com.starrocks.catalog.Database;
-import com.starrocks.catalog.IcebergTable;
 import com.starrocks.common.MetaNotFoundException;
 import com.starrocks.connector.exception.StarRocksConnectorException;
-import com.starrocks.connector.iceberg.cost.IcebergMetricsReporter;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
-import org.apache.iceberg.Transaction;
-import org.apache.iceberg.catalog.Namespace;
-import org.apache.iceberg.catalog.TableIdentifier;
-import org.apache.thrift.TException;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public interface IcebergCatalog {
 
     IcebergCatalogType getIcebergCatalogType();
-
-    Table loadTable(IcebergTable table) throws StarRocksConnectorException;
-
-    Table loadTable(TableIdentifier tableIdentifier) throws StarRocksConnectorException;
-
-    Table loadTable(TableIdentifier tableIdentifier, Optional<IcebergMetricsReporter> metricsReporter)
-            throws StarRocksConnectorException;
-
-    Table loadTable(TableIdentifier tableId,
-                    String tableLocation,
-                    Map<String, String> properties) throws StarRocksConnectorException;
 
     List<String> listAllDatabases();
 
@@ -55,27 +37,25 @@ public interface IcebergCatalog {
     default void dropDb(String dbName) throws MetaNotFoundException {
     }
 
-    Database getDB(String dbName) throws InterruptedException, TException;
+    Database getDB(String dbName);
 
-    List<TableIdentifier> listTables(Namespace of);
+    List<String> listTables(String dbName);
 
-    default Transaction newCreateTableTransaction(
-            String dbName,
-            String tableName,
-            Schema schema,
-            PartitionSpec partitionSpec,
-            String location,
-            Map<String, String> properties) {
-        throw new StarRocksConnectorException("This catalog doesn't support creating tables");
+    default boolean createTable(String dbName,
+                                String tableName,
+                                Schema schema,
+                                PartitionSpec partitionSpec,
+                                String location,
+                                Map<String, String> properties) {
+        return false;
     }
 
-    default boolean dropTable(TableIdentifier identifier, boolean purge) {
+    default boolean dropTable(String dbName, String tableName, boolean purge) {
         throw new StarRocksConnectorException("This catalog doesn't support dropping tables");
     }
 
-    default String defaultTableLocation(String dbName, String tblName) {
-        return "";
-    }
+    Table getTable(String dbName, String tableName) throws StarRocksConnectorException;
+
 
     default void deleteUncommittedDataFiles(List<String> fileLocations) {
     }

commit 1300e1e51bf51bbf67a6a1779ce9bf34812edf5d

[BugFix] Fix Iceberg S3FileIO not support assume role (#24054)
Fix Iceberg S3FileIO did not support assume role, this situation will
only happen when the user is using iceberg+glue.
The bug was introduced by
https://github.com/StarRocks/starrocks/pull/23618

Signed-off-by: Smith Cruise <chendingchao1@126.com>

fe/fe-core/src/main/java/com/starrocks/connector/iceberg/glue/IcebergAwsClientFactory.java
@@ -31,6 +31,7 @@ import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.StsClientBuilder;
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
 import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
@@ -79,7 +80,6 @@ public class IcebergAwsClientFactory implements AwsClientFactory {
     private String glueRegion;
     private String glueEndpoint;
 
-
     @Override
     public void initialize(Map<String, String> properties) {
         this.awsProperties = new AwsProperties(properties);
@@ -104,6 +104,33 @@ public class IcebergAwsClientFactory implements AwsClientFactory {
         glueEndpoint = properties.getOrDefault(AWS_GLUE_ENDPOINT, "");
     }
 
+    private StsAssumeRoleCredentialsProvider getAssumeRoleCredentialsProvider(AwsCredentialsProvider baseCredentials,
+                                                                              String iamRoleArn, String externalId,
+                                                                              String region) {
+        // Build sts client
+        StsClientBuilder stsClientBuilder = StsClient.builder().credentialsProvider(baseCredentials);
+        stsClientBuilder.applyMutation(awsProperties::applyHttpClientConfigurations);
+        if (!region.isEmpty()) {
+            stsClientBuilder.region(Region.of(region));
+        }
+
+        // Build AssumeRoleRequest
+        AssumeRoleRequest.Builder assumeRoleBuilder = AssumeRoleRequest.builder();
+        assumeRoleBuilder.roleArn(iamRoleArn);
+        assumeRoleBuilder.roleSessionName(UUID.randomUUID().toString());
+        if (!externalId.isEmpty()) {
+            assumeRoleBuilder.externalId(externalId);
+        }
+        // Below two configuration copied from Iceberg's official SDK
+        assumeRoleBuilder.durationSeconds(awsProperties.clientAssumeRoleTimeoutSec());
+        assumeRoleBuilder.tags(awsProperties.stsClientAssumeRoleTags());
+
+        return StsAssumeRoleCredentialsProvider.builder()
+                .stsClient(stsClientBuilder.build())
+                .refreshRequest(assumeRoleBuilder.build())
+                .build();
+    }
+
     @Override
     public S3Client s3() {
         if (s3UseAWSSDKDefaultBehavior) {
@@ -115,19 +142,8 @@ public class IcebergAwsClientFactory implements AwsClientFactory {
                 getBaseAWSCredentialsProvider(s3UseInstanceProfile, s3AccessKey, s3SecretKey);
         S3ClientBuilder s3ClientBuilder = S3Client.builder();
         if (!s3IamRoleArn.isEmpty()) {
-            StsClient stsClient = StsClient.builder().credentialsProvider(baseAWSCredentialsProvider).build();
-            String sessionName = UUID.randomUUID().toString();
-            AssumeRoleRequest.Builder assumeRoleBuilder = AssumeRoleRequest.builder();
-            assumeRoleBuilder.roleArn(s3IamRoleArn);
-            if (!s3ExternalId.isEmpty()) {
-                assumeRoleBuilder.externalId(s3ExternalId);
-            }
-            assumeRoleBuilder.roleSessionName(sessionName);
-            StsAssumeRoleCredentialsProvider.Builder builder = StsAssumeRoleCredentialsProvider.builder()
-                    .stsClient(stsClient)
-                    .refreshRequest(AssumeRoleRequest.builder()
-                            .build());
-            s3ClientBuilder.credentialsProvider(builder.build());
+            s3ClientBuilder.credentialsProvider(getAssumeRoleCredentialsProvider(baseAWSCredentialsProvider,
+                    s3IamRoleArn, s3ExternalId, s3Region));
         } else {
             s3ClientBuilder.credentialsProvider(baseAWSCredentialsProvider);
         }
@@ -156,19 +172,8 @@ public class IcebergAwsClientFactory implements AwsClientFactory {
                 getBaseAWSCredentialsProvider(glueUseInstanceProfile, glueAccessKey, glueSecretKey);
         GlueClientBuilder glueClientBuilder = GlueClient.builder();
         if (!glueIamRoleArn.isEmpty()) {
-            StsClient stsClient = StsClient.builder().credentialsProvider(baseAWSCredentialsProvider).build();
-            String sessionName = UUID.randomUUID().toString();
-            AssumeRoleRequest.Builder assumeRoleBuilder = AssumeRoleRequest.builder();
-            assumeRoleBuilder.roleArn(glueIamRoleArn);
-            if (!glueExternalId.isEmpty()) {
-                assumeRoleBuilder.externalId(glueExternalId);
-            }
-            assumeRoleBuilder.roleSessionName(sessionName);
-            StsAssumeRoleCredentialsProvider.Builder builder =
-                    StsAssumeRoleCredentialsProvider.builder()
-                            .stsClient(stsClient)
-                            .refreshRequest(AssumeRoleRequest.builder().build());
-            glueClientBuilder.credentialsProvider(builder.build());
+            glueClientBuilder.credentialsProvider(getAssumeRoleCredentialsProvider(baseAWSCredentialsProvider,
+                    glueIamRoleArn, glueExternalId, glueRegion));
         } else {
             glueClientBuilder.credentialsProvider(baseAWSCredentialsProvider);
         }