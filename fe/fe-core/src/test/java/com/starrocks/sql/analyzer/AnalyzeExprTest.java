deep-bi/starrocks

commit 46476458ca48511c6f3c7008e61a4b0211557cc5

[Enhancement] Support struct cast function (#24355)
Fixes #issue

* check and update struct cast function
* support index expression for struct
* add struct test

```

MySQL tpcds_1g> select row(1, 2, 3, 4)[1]
+--------------------+
| row(1, 2, 3, 4)[1] |
+--------------------+
|         1          |
+--------------------+

MySQL tpcds_1g> select cast(row(1, 2) as STRUCT<a string, b string>);
+-----------------------------------------------------------------+
| CAST((row(1, 2)) AS struct<a varchar(65533), b varchar(65533)>) |
+-----------------------------------------------------------------+
| {"a":"1","b":"2"}                                               |
+-----------------------------------------------------------------+
1 row in set
```

---------

Signed-off-by: Seaven <seaven_7@qq.com>

fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
@@ -438,10 +438,6 @@ public class ExpressionAnalyzer {
         public Void visitCollectionElementExpr(CollectionElementExpr node, Scope scope) {
             Expr expr = node.getChild(0);
             Expr subscript = node.getChild(1);
-            if (!expr.getType().isArrayType() && !expr.getType().isMapType()) {
-                throw new SemanticException("cannot subscript type " + expr.getType()
-                        + " because it is not an array or a map", expr.getPos());
-            }
             if (expr.getType().isArrayType()) {
                 if (!subscript.getType().isNumericType()) {
                     throw new SemanticException("array subscript must have type integer", subscript.getPos());
@@ -454,7 +450,7 @@ public class ExpressionAnalyzer {
                 } catch (AnalysisException e) {
                     throw new SemanticException(e.getMessage());
                 }
-            } else {
+            } else if (expr.getType().isMapType()) {
                 try {
                     if (subscript.getType().getPrimitiveType() !=
                             ((MapType) expr.getType()).getKeyType().getPrimitiveType()) {
@@ -464,6 +460,24 @@ public class ExpressionAnalyzer {
                 } catch (AnalysisException e) {
                     throw new SemanticException(e.getMessage());
                 }
+            } else if (expr.getType().isStructType()) {
+                if (!(subscript instanceof IntLiteral)) {
+                    throw new SemanticException("struct subscript must have integer pos", subscript.getPos());
+                }
+                long index = ((IntLiteral) subscript).getValue();
+                long fieldSize = ((StructType) expr.getType()).getFields().size();
+                if (fieldSize < Math.abs(index)) {
+                    throw new SemanticException("the pos is out of struct subfields", subscript.getPos());
+                } else if (index == 0) {
+                    throw new SemanticException("the pos can't set to zero", subscript.getPos());
+                }
+
+                index = index > 0 ? index - 1 : fieldSize + index;
+                StructField structField = ((StructType) expr.getType()).getFields().get((int) index);
+                node.setType(structField.getType());
+            } else {
+                throw new SemanticException("cannot subscript type " + expr.getType()
+                        + " because it is not an array or a map or a struct", expr.getPos());
             }
 
             return null;
@@ -1044,7 +1058,7 @@ public class ExpressionAnalyzer {
 
             for (int i = 0; i < fn.getNumArgs(); i++) {
                 if (!argumentTypes[i].matchesType(fn.getArgs()[i]) &&
-                        !Type.canCastToAsFunctionParameter(argumentTypes[i], fn.getArgs()[i])) {
+                        !Type.canCastTo(argumentTypes[i], fn.getArgs()[i])) {
                     String msg = String.format("No matching function with signature: %s(%s)", fnName,
                             node.getParams().isStar() ? "*" :
                                     Arrays.stream(argumentTypes).map(Type::toSql).collect(Collectors.joining(", ")));
@@ -1056,7 +1070,7 @@ public class ExpressionAnalyzer {
                 Type varType = fn.getArgs()[fn.getNumArgs() - 1];
                 for (int i = fn.getNumArgs(); i < argumentTypes.length; i++) {
                     if (!argumentTypes[i].matchesType(varType) &&
-                            !Type.canCastToAsFunctionParameter(argumentTypes[i], varType)) {
+                            !Type.canCastTo(argumentTypes[i], varType)) {
                         String msg = String.format("Variadic function %s(%s) can't support type: %s", fnName,
                                 Arrays.stream(fn.getArgs()).map(Type::toSql).collect(Collectors.joining(", ")),
                                 argumentTypes[i]);

fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeExprTest.java
@@ -337,4 +337,12 @@ public class AnalyzeExprTest {
         analyzeFail("select map_concat()");
     }
 
+    @Test
+    public void testAnalyzeStructFunc() {
+        analyzeFail("select row('a', 1, 'b', 2)[0]");
+        analyzeFail("select row('a', 1, 'b', 2)[5]");
+        analyzeFail("select row('a', 1, 'b', 2)[-5]");
+        analyzeSuccess("select row('a', 1, 'b', 2)[1]");
+        analyzeSuccess("select row('a', 1, 'b', 2)[-1]");
+    }
 }

be/src/exprs/cast_expr.cpp
@@ -1621,6 +1621,9 @@ StatusOr<std::unique_ptr<Expr>> VectorizedCastExprFactory::create_cast_expr(Obje
         if (from_type.children.size() != to_type.children.size()) {
             return Status::NotSupported("vectorized engine not support cast struct with different number of children.");
         }
+        if (to_type.field_names.empty() || from_type.field_names.size() != to_type.field_names.size()) {
+            return Status::NotSupported("vectorized engine not support cast struct with different field of children.");
+        }
         std::vector<std::unique_ptr<Expr>> field_casts{from_type.children.size()};
         for (int i = 0; i < from_type.children.size(); ++i) {
             ASSIGN_OR_RETURN(field_casts[i],

be/src/exprs/cast_nested.cpp
@@ -82,7 +82,8 @@ StatusOr<ColumnPtr> CastStructExpr::evaluate_checked(ExprContext* context, Chunk
             casted_fields.emplace_back(struct_column->fields()[i]->clone_shared());
         }
     }
-    auto casted_struct = StructColumn::create(std::move(casted_fields));
+
+    auto casted_struct = StructColumn::create(std::move(casted_fields), _type.field_names);
     RETURN_IF_ERROR(casted_struct->unfold_const_children(_type));
     if (!orig_column->is_nullable()) {
         return std::move(casted_struct);

fe/fe-core/src/main/java/com/starrocks/analysis/SubfieldExpr.java
@@ -25,28 +25,30 @@ import com.starrocks.sql.parser.NodePosition;
 import com.starrocks.thrift.TExprNode;
 import com.starrocks.thrift.TExprNodeType;
 
+import java.util.List;
+
 public class SubfieldExpr extends Expr {
 
     // We use fieldNames to extract subfield column from children[0],
     // children[0] must be an StructType.
-    private final ImmutableList<String> fieldNames;
+    private final List<String> fieldNames;
 
     // Only used in parser, in parser, we can't determine column's type
-    public SubfieldExpr(Expr child, ImmutableList<String> fieldNames) {
+    public SubfieldExpr(Expr child, List<String> fieldNames) {
         this(child, null, fieldNames);
     }
 
-    public SubfieldExpr(Expr child, ImmutableList<String> fieldNames, NodePosition pos) {
+    public SubfieldExpr(Expr child, List<String> fieldNames, NodePosition pos) {
         this(child, null, fieldNames, pos);
     }
 
     // In this constructor, we can determine column's type
     // child must be an StructType
-    public SubfieldExpr(Expr child, Type type, ImmutableList<String> fieldNames) {
+    public SubfieldExpr(Expr child, Type type, List<String> fieldNames) {
         this(child, type, fieldNames, NodePosition.ZERO);
     }
 
-    public SubfieldExpr(Expr child, Type type, ImmutableList<String> fieldNames, NodePosition pos) {
+    public SubfieldExpr(Expr child, Type type, List<String> fieldNames, NodePosition pos) {
         super(pos);
         if (type != null) {
             Preconditions.checkArgument(child.getType().isStructType());
@@ -61,7 +63,7 @@ public class SubfieldExpr extends Expr {
         fieldNames = other.fieldNames;
     }
 
-    public ImmutableList<String> getFieldNames() {
+    public List<String> getFieldNames() {
         return fieldNames;
     }
 

fe/fe-core/src/main/java/com/starrocks/catalog/StructType.java
@@ -32,6 +32,7 @@ import com.starrocks.sql.analyzer.SemanticException;
 import com.starrocks.thrift.TTypeDesc;
 import com.starrocks.thrift.TTypeNode;
 import com.starrocks.thrift.TTypeNodeType;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -125,6 +126,9 @@ public class StructType extends Type {
         }
         for (int i = 0; i < fields.size(); ++i) {
             if (!fields.get(i).getType().matchesType(rhsType.fields.get(i).getType())) {
+                return false;
+            }
+            if (!StringUtils.equals(fields.get(i).getName(), rhsType.fields.get(i).getName())) {
                 return false;
             }
         }

fe/fe-core/src/main/java/com/starrocks/catalog/Type.java
@@ -1051,20 +1051,6 @@ public abstract class Type implements Cloneable {
         }
     }
 
-    public static boolean canCastToAsFunctionParameter(Type from, Type to) {
-        if (from.isNull()) {
-            return true;
-        } else if (from.isScalarType() && to.isScalarType()) {
-            return ScalarType.canCastTo((ScalarType) from, (ScalarType) to);
-        } else if (from.isArrayType() && to.isArrayType()) {
-            return canCastTo(((ArrayType) from).getItemType(), ((ArrayType) to).getItemType());
-        } else if (from.isMapType() && to.isMapType()) {
-            return canCastTo(from, to);
-        } else {
-            return false;
-        }
-    }
-
     public static boolean canCastTo(Type from, Type to) {
         if (from.isNull()) {
             return true;

commit 42e18ca56b5cf79516380f4ee72e457513267d28

[BugFix] array func's inputs should be array (#25066)
Fixes https://github.com/StarRocks/StarRocksTest/issues/2678

caused by https://github.com/StarRocks/starrocks/pull/24355, which
allows cast string to array, but can't know the specific type of array
items, leading to erros.

---------

Signed-off-by: Zhuhe Fang <fzhedu@gmail.com>

fe/fe-core/src/main/java/com/starrocks/catalog/FunctionSet.java
@@ -294,6 +294,11 @@ public class FunctionSet {
     public static final String ARRAY_APPEND = "array_append";
     public static final String ARRAY_AVG = "array_avg";
     public static final String ARRAY_CONTAINS = "array_contains";
+    public static final String ARRAY_CONTAINS_ALL = "array_contains_all";
+    public static final String ARRAY_CUM_SUM = "array_cum_sum";
+
+
+
     public static final String ARRAY_JOIN = "array_join";
     public static final String ARRAY_DISTINCT = "array_distinct";
     public static final String ARRAY_LENGTH = "array_length";
@@ -309,6 +314,8 @@ public class FunctionSet {
     public static final String ALL_MATCH = "all_match";
 
     public static final String ARRAY_GENERATE = "array_generate";
+
+    public static final String ARRAY_TO_BITMAP = "array_to_bitmap";
 
     // Bit functions:
     public static final String BITAND = "bitand";

fe/fe-core/src/main/java/com/starrocks/sql/analyzer/ExpressionAnalyzer.java
@@ -1239,6 +1239,70 @@ public class ExpressionAnalyzer {
                     }
                     break;
                 }
+                case FunctionSet.ARRAY_AVG:
+                case FunctionSet.ARRAY_MAX:
+                case FunctionSet.ARRAY_MIN:
+                case FunctionSet.ARRAY_SORT:
+                case FunctionSet.ARRAY_SUM:
+                case FunctionSet.ARRAY_CUM_SUM:
+                case FunctionSet.ARRAY_DIFFERENCE:
+                case FunctionSet.ARRAY_DISTINCT:
+                case FunctionSet.ARRAY_LENGTH:
+                case FunctionSet.ARRAY_TO_BITMAP: {
+                    if (node.getChildren().size() != 1) {
+                        throw new SemanticException(fnName + " should have only one input", node.getPos());
+                    }
+                    if (!node.getChild(0).getType().isArrayType() && !node.getChild(0).getType().isNull()) {
+                        throw new SemanticException("The only one input of " + fnName +
+                                " should be an array, rather than " + node.getChild(0).getType().toSql(),
+                                node.getPos());
+                    }
+
+                    break;
+                }
+                case FunctionSet.ARRAY_CONTAINS_ALL:
+                case FunctionSet.ARRAYS_OVERLAP: {
+                    if (node.getChildren().size() != 2) {
+                        throw new SemanticException(fnName + " should have only two inputs", node.getPos());
+                    }
+                    for (int i = 0; i < node.getChildren().size(); ++i) {
+                        if (!node.getChild(i).getType().isArrayType() && !node.getChild(i).getType().isNull()) {
+                            throw new SemanticException((i + 1) + "-th input of " + fnName +
+                                    " should be an array, rather than " + node.getChild(i).getType().toSql(),
+                                    node.getPos());
+                        }
+                    }
+                    break;
+                }
+                case FunctionSet.ARRAY_INTERSECT:
+                case FunctionSet.ARRAY_CONCAT: {
+                    if (node.getChildren().isEmpty()) {
+                        throw new SemanticException(fnName + " should have at least one input.", node.getPos());
+                    }
+                    for (int i = 0; i < node.getChildren().size(); ++i) {
+                        if (!node.getChild(i).getType().isArrayType() && !node.getChild(i).getType().isNull()) {
+                            throw new SemanticException((i + 1) + "-th input of " + fnName +
+                                    " should be an array, rather than " + node.getChild(i).getType().toSql(),
+                                    node.getPos());
+                        }
+                    }
+                    break;
+                }
+                case FunctionSet.ARRAY_CONTAINS:
+                case FunctionSet.ARRAY_APPEND:
+                case FunctionSet.ARRAY_JOIN:
+                case FunctionSet.ARRAY_POSITION:
+                case FunctionSet.ARRAY_REMOVE:
+                case FunctionSet.ARRAY_SLICE: {
+                    if (node.getChildren().isEmpty()) {
+                        throw new SemanticException(fnName + " should have at least one input.", node.getPos());
+                    }
+                    if (!node.getChild(0).getType().isArrayType() && !node.getChild(0).getType().isNull()) {
+                        throw new SemanticException("The first input of " + fnName +
+                                " should be an array", node.getPos());
+                    }
+                    break;
+                }
             }
         }
 

fe/fe-core/src/test/java/com/starrocks/sql/analyzer/AnalyzeExprTest.java
@@ -389,4 +389,56 @@ public class AnalyzeExprTest {
         analyzeSuccess("select row('a', 1, 'b', 2)[1]");
         analyzeSuccess("select row('a', 1, 'b', 2)[-1]");
     }
+
+    @Test
+    public void testAnalyzeArrayFunc() {
+        analyzeFail("select array_append('aaa','a')");
+        analyzeFail("select array_avg('aaa')");
+        analyzeFail("select array_concat('aaa','a')");
+        analyzeFail("select array_contains('abc','a')");
+        analyzeFail("select array_contains_all('abc','[]')");
+        analyzeFail("select array_cum_sum('arr')");
+        analyzeFail("select array_difference('aaa')");
+        analyzeFail("select ARRAY_DISTINCT('aa')");
+        analyzeFail("select array_filter('a','b')");
+        analyzeFail("select array_intersect('b','bb')");
+        analyzeFail("select ARRAY_JOIN('abc','-')");
+        analyzeFail("select array_length('abc')");
+        analyzeFail("select array_map('abc', x->upper(x))");
+        analyzeFail("select array_max('abc')");
+        analyzeFail("select array_min('bcd')");
+        analyzeFail("select arrays_overlap('abc','ab')");
+        analyzeFail("select array_position('abc','a')");
+        analyzeFail("select array_remove('abc','a')");
+        analyzeFail("select array_slice('abc', 1,2)");
+        analyzeFail("select ARRAY_SORT('abc')");
+        analyzeFail("select array_sortby('abc','b')");
+        analyzeFail("select array_sum('abc')");
+        analyzeFail("select array_to_bitmap('abc')");
+
+        analyzeFail("select array_append('[1,2]','a')");
+        analyzeFail("select array_avg('[1,2]')");
+        analyzeFail("select array_concat('[1,2]','a')");
+        analyzeFail("select array_contains('[1,2]','a')");
+        analyzeFail("select array_contains_all('[1,2]','[1]')");
+        analyzeFail("select array_cum_sum('[1,2]')");
+        analyzeFail("select array_difference('[1,2]')");
+        analyzeFail("select ARRAY_DISTINCT('[1,2]')");
+        analyzeFail("select array_filter('[1,2]','[1,2]')");
+        analyzeFail("select array_intersect('[1,2]','[1]')");
+        analyzeFail("select ARRAY_JOIN('[1,2]','-')");
+        analyzeFail("select array_length('[1,2]')");
+        analyzeFail("select array_map('[1,2]', x->upper(x))");
+        analyzeFail("select array_max('[1,2]')");
+        analyzeFail("select array_min('[1,2]')");
+        analyzeFail("select arrays_overlap('[1,2]','[1,2,4]')");
+        analyzeFail("select array_position('[1,2]','1')");
+        analyzeFail("select array_remove('[1,2]','1')");
+        analyzeFail("select array_slice('[1,3,2]', 1,2)");
+        analyzeFail("select ARRAY_SORT('[1,2]')");
+        analyzeFail("select array_sortby('[a,b]','[1,2]')");
+        analyzeFail("select array_sum('[1,2]')");
+        analyzeFail("select array_to_bitmap('[1,2]')");
+    }
+
 }

fe/fe-core/src/test/java/com/starrocks/sql/plan/ArrayTypeTest.java
@@ -248,7 +248,6 @@ public class ArrayTypeTest extends PlanTestBase {
     public void testArrayDifferenceArgs2() throws Exception {
         String sql = "select array_difference(c0) from test_array";
         expectedEx.expect(SemanticException.class);
-        expectedEx.expectMessage("No matching function with signature: array_difference(int(11)).");
         getFragmentPlan(sql);
     }
 
@@ -256,8 +255,6 @@ public class ArrayTypeTest extends PlanTestBase {
     public void testArrayDifferenceArgs3() throws Exception {
         String sql = "select array_difference(c1, 3) from test_array";
         expectedEx.expect(SemanticException.class);
-        expectedEx.expectMessage(
-                "No matching function with signature: array_difference(array<varchar(65533)>, tinyint(4)).");
         getFragmentPlan(sql);
     }
 

test/sql/test_array_fn/R/test_array_fn
@@ -686,7 +686,7 @@ select array_agg(i_1) from array_test;
 -- !result
 select array_agg(f_1) from array_test;
 -- result:
-[[10,20,30,4,100,10.1,2.1,30.2,40.3,-1,-10,100],null,[1,2,3,4,10,1.1,2.1,3.2,4.3,-1,-10,100],[1,2,3,4,10,null,1.1,2.1,3.2,null,4.3,-1,-10,100]]
+[[1,2,3,4,10,1.1,2.1,3.2,4.3,-1,-10,100],[1,2,3,4,10,null,1.1,2.1,3.2,null,4.3,-1,-10,100],[10,20,30,4,100,10.1,2.1,30.2,40.3,-1,-10,100],null]
 -- !result
 select array_agg(d_1) from array_test;
 -- result:
@@ -702,7 +702,7 @@ select array_agg(d_3) from array_test;
 -- !result
 select array_agg(d_4) from array_test;
 -- result:
-[null,[4.00000,2.10000,3.20000,10.00000,20.00000,3.00000,1.10000,-10.00000,-100.00000,100.00000,10.00000,4.30000],[4.00000,2.10000,3.20000,10.00000,2.00000,3.00000,1.10000,-1.00000,-10.00000,100.00000,1.00000,4.30000],[4.00000,2.10000,3.20000,10.00000,2.00000,3.00000,1.10000,-1.00000,-10.00000,100.00000,1.00000,4.30000]]
+[[4.00000,2.10000,3.20000,10.00000,2.00000,3.00000,1.10000,-1.00000,-10.00000,100.00000,1.00000,4.30000],[4.00000,2.10000,3.20000,10.00000,2.00000,3.00000,1.10000,-1.00000,-10.00000,100.00000,1.00000,4.30000],null,[4.00000,2.10000,3.20000,10.00000,20.00000,3.00000,1.10000,-10.00000,-100.00000,100.00000,10.00000,4.30000]]
 -- !result
 select array_agg(d_5) from array_test;
 -- result:
@@ -718,7 +718,7 @@ select array_agg(ai_1) from array_test;
 -- !result
 select array_agg(as_1) from array_test;
 -- result:
-[null,[["1","20","3","4"],["-1","a00","-100","100"],["a","b0","c"]],[["1","2","3","4"],["-1","a","-100","100"],["a","b","c"]],[["1","2","3","4"],["-1","a","-100","100"],["a","b","c"]]]
+[[["1","2","3","4"],["-1","a","-100","100"],["a","b","c"]],[["1","2","3","4"],["-1","a","-100","100"],["a","b","c"]],null,[["1","20","3","4"],["-1","a00","-100","100"],["a","b0","c"]]]
 -- !result
 select array_agg(aas_1) from array_test;
 -- result:
@@ -1443,35 +1443,35 @@ E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 43. D
 -- !result
 select arrays_overlap(s_1, 1) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 28. Detail message: No matching function with signature: arrays_overlap(array<varchar(65533)>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 28. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap(d_1, 3) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 28. Detail message: No matching function with signature: arrays_overlap(array<decimal128(26, 2)>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 28. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap(as_1, 100) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 31. Detail message: No matching function with signature: arrays_overlap(array<array<varchar(65533)>>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 31. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap(aas_1, -10) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 32. Detail message: No matching function with signature: arrays_overlap(array<array<array<varchar(65533)>>>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 32. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap(NULL, -1) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 30. Detail message: No matching function with signature: arrays_overlap(NULL_TYPE, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 30. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap([1.0,2.1,3.2,4.3], 1) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 42. Detail message: No matching function with signature: arrays_overlap(array<decimal32(2, 1)>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 42. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap(['a', 'b', 'c'], 1) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 40. Detail message: No matching function with signature: arrays_overlap(array<varchar>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 40. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap([[1,2,3], [2,3,4]], 3) from array_test order by pk;
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 43. Detail message: No matching function with signature: arrays_overlap(array<array<tinyint(4)>>, tinyint(4)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 43. Detail message: 2-th input of arrays_overlap should be an array, rather than tinyint(4).')
 -- !result
 select arrays_overlap(s_1, s_1) from array_test order by pk;
 -- result:
@@ -4012,7 +4012,7 @@ select array_contains( cast ('[40360,40361]' as array<int>), 40360);
 -- !result
 select array_contains_all( cast ('[40360,40361]' as array<int>), 40360);
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 70. Detail message: No matching function with signature: array_contains_all(array<int(11)>, int(11)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 70. Detail message: 2-th input of array_contains_all should be an array, rather than int(11).')
 -- !result
 select array_append( cast ('[40360,40361]' as array<int>), 40360);
 -- result:
@@ -4020,7 +4020,7 @@ select array_append( cast ('[40360,40361]' as array<int>), 40360);
 -- !result
 select array_avg( cast ('[40360,40361]' as array<int>), 40360);
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 61. Detail message: No matching function with signature: array_avg(array<int(11)>, int(11)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 61. Detail message: array_avg should have only one input.')
 -- !result
 select array_concat( cast ('[40360,40361]' as array<int>), [40360]);
 -- result:
@@ -4116,7 +4116,7 @@ None
 -- !result
 select array_contains_all( cast ('null' as array<int>), 40360);
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 61. Detail message: No matching function with signature: array_contains_all(array<int(11)>, int(11)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 61. Detail message: 2-th input of array_contains_all should be an array, rather than int(11).')
 -- !result
 select array_append( cast ('null' as array<int>), 40360);
 -- result:
@@ -4124,7 +4124,7 @@ None
 -- !result
 select array_avg( cast ('null' as array<int>), 40360);
 -- result:
-E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 52. Detail message: No matching function with signature: array_avg(array<int(11)>, int(11)).')
+E: (1064, 'Getting analyzing error from line 1, column 7 to line 1, column 52. Detail message: array_avg should have only one input.')
 -- !result
 select array_concat( cast ('null' as array<int>), [40360]);
 -- result: