deep-bi/starrocks

commit 8fac0c9b5a2af12623b895e452b29713afa23a29

[Refactor] refactor spill trigger strategy [1/2] (#24644)
A concept of reserve memory is provided.
Before executing push_chunk/set_finishing, we need to reserve memory in
advance for operators that requests a large amount of memory. If the
reserve fails, the operator will try to spill.

Signed-off-by: stdpain <drfeng08@gmail.com>

be/src/exec/pipeline/pipeline_driver.cpp
@@ -26,6 +26,7 @@
 #include "exec/query_cache/multilane_operator.h"
 #include "exec/query_cache/ticket_checker.h"
 #include "exec/workgroup/work_group.h"
+#include "runtime/current_thread.h"
 #include "runtime/exec_env.h"
 #include "runtime/runtime_state.h"
 #include "util/debug/query_trace.h"
@@ -245,6 +246,7 @@ StatusOr<DriverState> PipelineDriver::process(RuntimeState* runtime_state, int w
         });
 
         SCOPED_RAW_TIMER(&process_time_ns);
+        auto query_mem_tracker = _query_ctx->mem_tracker();
 
         for (size_t i = _first_unfinished; i < num_operators - 1; ++i) {
             {
@@ -258,6 +260,8 @@ StatusOr<DriverState> PipelineDriver::process(RuntimeState* runtime_state, int w
                         // For source operators
                         RETURN_IF_ERROR(return_status = _mark_operator_finishing(curr_op, runtime_state));
                     }
+                    _adjust_memory_usage(runtime_state, query_mem_tracker.get(), next_op, nullptr);
+                    RELEASE_RESERVED_GUARD();
                     RETURN_IF_ERROR(return_status = _mark_operator_finishing(next_op, runtime_state));
                     new_first_unfinished = i + 1;
                     continue;
@@ -270,21 +274,6 @@ StatusOr<DriverState> PipelineDriver::process(RuntimeState* runtime_state, int w
 
                 if (_check_fragment_is_canceled(runtime_state)) {
                     return _state;
-                }
-
-                // Run spill stragety
-                // TODO: FIXME
-                // a simple spill stragety
-                auto query_mem_tracker = _query_ctx->mem_tracker();
-                if (runtime_state->enable_spill() &&
-                    sink_operator()->revocable_mem_bytes() > runtime_state->spill_operator_min_bytes() &&
-                    !sink_operator()->need_mark_spill()) {
-                    auto spill_manager = _query_ctx->spill_manager();
-                    if (query_mem_tracker->consumption() - spill_manager->pending_spilled_bytes() >
-                        query_mem_tracker->limit() * runtime_state->spill_mem_limit_threshold()) {
-                        spill_manager->update_spilled_bytes(sink_operator()->revocable_mem_bytes());
-                        sink_operator()->mark_need_spill();
-                    }
                 }
 
                 // pull chunk from current operator and push the chunk onto next
@@ -317,6 +306,8 @@ StatusOr<DriverState> PipelineDriver::process(RuntimeState* runtime_state, int w
                             SCOPED_THREAD_LOCAL_OPERATOR_MEM_TRACKER_SETTER(next_op);
                             SCOPED_TIMER(next_op->_push_timer);
                             QUERY_TRACE_SCOPED(next_op->get_name(), "push_chunk");
+                            _adjust_memory_usage(runtime_state, query_mem_tracker.get(), next_op, maybe_chunk.value());
+                            RELEASE_RESERVED_GUARD();
                             return_status = next_op->push_chunk(runtime_state, maybe_chunk.value());
                         }
                         // ignore empty chunk generated by per-tablet computation when query cache enabled
@@ -344,6 +335,8 @@ StatusOr<DriverState> PipelineDriver::process(RuntimeState* runtime_state, int w
                         // For source operators
                         RETURN_IF_ERROR(return_status = _mark_operator_finishing(curr_op, runtime_state));
                     }
+                    _adjust_memory_usage(runtime_state, query_mem_tracker.get(), next_op, nullptr);
+                    RELEASE_RESERVED_GUARD();
                     RETURN_IF_ERROR(return_status = _mark_operator_finishing(next_op, runtime_state));
                     new_first_unfinished = i + 1;
                     continue;
@@ -463,6 +456,28 @@ void PipelineDriver::cancel_operators(RuntimeState* runtime_state) {
 void PipelineDriver::_close_operators(RuntimeState* runtime_state) {
     for (auto& op : _operators) {
         _mark_operator_closed(op, runtime_state);
+    }
+}
+
+void PipelineDriver::_adjust_memory_usage(RuntimeState* state, MemTracker* tracker, OperatorPtr& op,
+                                          const ChunkPtr& chunk) {
+    // TODO: FIXME
+    // a simple spill stragety
+    auto& mem_resource_mgr = op->mem_resource_manager();
+    if (state->enable_spill() && mem_resource_mgr.releaseable() &&
+        op->revocable_mem_bytes() > state->spill_operator_min_bytes()) {
+        auto request_reserved = 0;
+        if (chunk == nullptr) {
+            request_reserved = op->estimated_memory_reserved();
+        } else {
+            request_reserved = op->estimated_memory_reserved(chunk);
+        }
+        request_reserved += state->spill_mem_table_num() * state->spill_mem_table_size();
+
+        if (!tls_thread_status.try_mem_reserve(request_reserved, tracker,
+                                               tracker->limit() * state->spill_mem_limit_threshold())) {
+            mem_resource_mgr.to_low_memory_mode();
+        }
     }
 }
 

be/src/exec/spillable_chunks_sorter_full_sort.cpp
@@ -116,6 +116,13 @@ Status SpillableChunksSorterFullSort::get_next(ChunkPtr* chunk, bool* eos) {
     return Status::OK();
 }
 
+size_t SpillableChunksSorterFullSort::reserved_bytes(const ChunkPtr& chunk) {
+    if (chunk) {
+        return chunk->memory_usage() + _unsorted_chunk->memory_usage() * 2;
+    }
+    return _unsorted_chunk->memory_usage() * 2;
+}
+
 size_t SpillableChunksSorterFullSort::get_output_rows() const {
     if (!_spiller->spilled()) {
         return ChunksSorterFullSort::get_output_rows();

be/src/runtime/current_thread.h
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 #include "fmt/format.h"
@@ -61,6 +62,7 @@ private:
         MemCacheManager(MemCacheManager&&) = delete;
 
         void consume(int64_t size) {
+            size = _consume_from_reserved(size);
             _cache_size += size;
             _allocated_cache_size += size;
             _total_consumed_bytes += size;
@@ -71,6 +73,7 @@ private:
 
         bool try_mem_consume(int64_t size) {
             MemTracker* cur_tracker = _loader();
+            size = _consume_from_reserved(size);
             _cache_size += size;
             _allocated_cache_size += size;
             _total_consumed_bytes += size;
@@ -88,6 +91,45 @@ private:
                 }
             }
             return true;
+        }
+
+        bool try_mem_consume_with_limited_tracker(int64_t size, MemTracker* tracker, int64_t limit) {
+            MemTracker* cur_tracker = _loader();
+            size = _consume_from_reserved(size);
+            _cache_size += size;
+            _allocated_cache_size += size;
+            _total_consumed_bytes += size;
+            if (cur_tracker != nullptr && _cache_size >= BATCH_SIZE) {
+                MemTracker* limit_tracker = cur_tracker->try_consume_with_limited(_cache_size, tracker, limit);
+                if (LIKELY(limit_tracker == nullptr)) {
+                    _cache_size = 0;
+                    return true;
+                } else {
+                    _cache_size -= size;
+                    _allocated_cache_size -= size;
+                    _try_consume_mem_size = size;
+                    tls_exceed_mem_tracker = limit_tracker;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool try_mem_reserve(int64_t reserve_bytes, MemTracker* tracker, int64_t limit) {
+            DCHECK(_reserved_bytes == 0);
+            DCHECK(reserve_bytes >= 0);
+            if (try_mem_consume_with_limited_tracker(reserve_bytes, tracker, limit)) {
+                _reserved_bytes = reserve_bytes;
+                return true;
+            }
+            return false;
+        }
+
+        void release_reserved() {
+            if (_reserved_bytes) {
+                release(_reserved_bytes);
+                _reserved_bytes = 0;
+            }
         }
 
         void release(int64_t size) {
@@ -124,9 +166,22 @@ private:
         int64_t get_consumed_bytes() const { return _total_consumed_bytes; }
 
     private:
+        int64_t _consume_from_reserved(int64_t size) {
+            if (_reserved_bytes > size) {
+                _reserved_bytes -= size;
+                size = 0;
+            } else {
+                size -= _reserved_bytes;
+                _reserved_bytes = 0;
+            }
+            return size;
+        }
+
         const static int64_t BATCH_SIZE = 2 * 1024 * 1024;
 
         std::function<MemTracker*()> _loader;
+
+        int64_t _reserved_bytes = 0;
 
         // Allocated or delocated but not committed memory bytes, can be negative
         int64_t _cache_size = 0;
@@ -212,6 +267,15 @@ public:
         }
         return false;
     }
+
+    bool try_mem_reserve(int64_t size, MemTracker* tracker, int64_t limit) {
+        if (_mem_cache_manager.try_mem_reserve(size, tracker, limit)) {
+            return true;
+        }
+        return false;
+    }
+
+    void release_reserved() { _mem_cache_manager.release_reserved(); }
 
     void mem_release(int64_t size) {
         _mem_cache_manager.release(size);
@@ -356,6 +420,9 @@ private:
 };
 
 #define SCOPED_SET_CATCHED(catched) auto VARNAME_LINENUM(catched_setter) = CurrentThreadCatchSetter(catched)
+
+#define RELEASE_RESERVED_GUARD() \
+    auto VARNAME_LINENUM(defer) = DeferOp([] { CurrentThread::current().release_reserved(); });
 
 #define SET_TRACE_INFO(driver_id, query_id, fragment_instance_id) \
     CurrentThread::current().set_pipeline_driver_id(driver_id);   \

be/src/exec/CMakeLists.txt
@@ -273,6 +273,7 @@ set(EXEC_FILES
     spill/serde.cpp
     spill/input_stream.cpp
     spill/log_block_manager.cpp
+    spill/operator_mem_resource_manager.cpp
     stream/state/mem_state_table.cpp
     stream/aggregate/agg_state_data.cpp
     stream/aggregate/agg_group_state.cpp

be/src/exec/chunks_sorter.h
@@ -148,6 +148,8 @@ public:
     size_t revocable_mem_bytes() const { return _revocable_mem_bytes; }
     void set_spill_stragety(spill::SpillStrategy stragety) { _spill_strategy = stragety; }
 
+    virtual size_t reserved_bytes(const ChunkPtr& chunk) { return chunk != nullptr ? chunk->memory_usage() : 0; }
+
     virtual void cancel() {}
 
 protected:

be/src/exec/chunks_sorter_full_sort.cpp
@@ -103,6 +103,7 @@ Status ChunksSorterFullSort::_partial_sort(RuntimeState* state, bool done) {
         _max_num_rows = std::max<int>(_max_num_rows, _staging_unsorted_rows);
         _profiler->input_required_memory->update(_staging_unsorted_bytes);
         concat_chunks(_unsorted_chunk, _staging_unsorted_chunks, _staging_unsorted_rows);
+        _staging_unsorted_chunks.clear();
         RETURN_IF_ERROR(_unsorted_chunk->upgrade_if_overflow());
 
         SCOPED_TIMER(_sort_timer);
@@ -119,7 +120,6 @@ Status ChunksSorterFullSort::_partial_sort(RuntimeState* state, bool done) {
         _unsorted_chunk->reset();
         _staging_unsorted_rows = 0;
         _staging_unsorted_bytes = 0;
-        _staging_unsorted_chunks.clear();
     }
 
     return Status::OK();

be/src/exec/pipeline/aggregate/aggregate_distinct_streaming_sink_operator.cpp
@@ -48,6 +48,10 @@ StatusOr<ChunkPtr> AggregateDistinctStreamingSinkOperator::pull_chunk(RuntimeSta
     return Status::InternalError("Not support");
 }
 
+void AggregateDistinctStreamingSinkOperator::set_execute_mode(int performance_level) {
+    _aggregator->streaming_preaggregation_mode() = TStreamingPreaggregationMode::FORCE_STREAMING;
+}
+
 Status AggregateDistinctStreamingSinkOperator::push_chunk(RuntimeState* state, const ChunkPtr& chunk) {
     size_t chunk_size = chunk->num_rows();
 

be/src/exec/pipeline/aggregate/aggregate_distinct_streaming_sink_operator.h
@@ -44,6 +44,9 @@ public:
     Status push_chunk(RuntimeState* state, const ChunkPtr& chunk) override;
     Status reset_state(RuntimeState* state, const std::vector<ChunkPtr>& refill_chunks) override;
 
+    bool releaseable() const override { return true; }
+    void set_execute_mode(int performance_level) override;
+
 private:
     // Invoked by push_chunk if current mode is TStreamingPreaggregationMode::FORCE_STREAMING
     Status _push_chunk_by_force_streaming(const ChunkPtr& chunk);

commit 4b17dd2b5ee8340e07455f2b1fe2d36af4305c55

[BugFix] fix wrong consumption in mem_tracker under auto spill mode (#25657)
## BackGround
when I test auto spill mode under tpch-1T dataset in a 16c64g instance,
I found Q18 will cancel due to OOM, and the consumption values of some
mem tracker are wrong after the query cancels:

1.  the consumption of chunk_allocator will be a negative number 

2.  the consumption of process mem tracker will be a very large number

these issues were introduced by
https://github.com/StarRocks/starrocks/pull/24644

### for the first issue

After the reserved_bytes is introduced, the mem tracker will be updated
in advance, and the unused reserved memory will be returned after the
push_chunk/set_finishing call ends. So when push_chunk/set_finishing is
executed, if the allocated memory size does not exceed reserved bytes in
current thread, then the mem tracker will not be updated.

If the tls_mem_tracker doesn't switch during execution, everything is
ok. Otherwise there will be problems, take MemChunkAllocator as an
example:

![image](https://github.com/StarRocks/starrocks/assets/3675229/1b7de188-7958-48f4-99c9-1beafb2fdfcb)


consider the following operations:
1. first, reserving memory in PipelineDriver::_adjust_memory_usage,
tls_mem_tracker is fragment instance tracker, if we reserve 10 bytes,
fragment->query->wg->query_pool->process will be updated.
4. then executing MemChunkAllocator::allocate, tls_mem_tracker will
switch to chunk_allocator, if we allocate 5 bytes, chunk_allocator
tracker won't be really update because of the existence of
reserved_bytes in current thread
5. in the defer op, because of the existence of reserve bytes, we cannot
know how much the consumption of chunk_allocator tracker has been
updated. At this time, we still release 5 bytes, which is wrong.

The solution is to release the reserved memory every time the tracker is
switched.


### for the second issue

be/src/exec/pipeline/pipeline_driver.cpp
@@ -468,7 +468,7 @@ void PipelineDriver::_adjust_memory_usage(RuntimeState* state, MemTracker* track
     auto& mem_resource_mgr = op->mem_resource_manager();
     if (state->enable_spill() && mem_resource_mgr.releaseable() &&
         op->revocable_mem_bytes() > state->spill_operator_min_bytes()) {
-        auto request_reserved = 0;
+        int64_t request_reserved = 0;
         if (chunk == nullptr) {
             request_reserved = op->estimated_memory_reserved();
         } else {

be/src/exec/spillable_chunks_sorter_full_sort.cpp
@@ -111,9 +111,9 @@ Status SpillableChunksSorterFullSort::get_next(ChunkPtr* chunk, bool* eos) {
 
 size_t SpillableChunksSorterFullSort::reserved_bytes(const ChunkPtr& chunk) {
     if (chunk) {
-        return chunk->memory_usage() + _unsorted_chunk->memory_usage() * 2;
+        return chunk->memory_usage() + (_unsorted_chunk != nullptr ? _unsorted_chunk->memory_usage() * 2 : 0);
     }
-    return _unsorted_chunk->memory_usage() * 2;
+    return _unsorted_chunk != nullptr ? _unsorted_chunk->memory_usage() * 2 : 0;
 }
 
 size_t SpillableChunksSorterFullSort::get_output_rows() const {

be/src/runtime/current_thread.h
@@ -218,6 +218,7 @@ public:
 
     // Return prev memory tracker.
     starrocks::MemTracker* set_mem_tracker(starrocks::MemTracker* mem_tracker) {
+        release_reserved();
         mem_tracker_ctx_shift();
         auto* prev = tls_mem_tracker;
         tls_mem_tracker = mem_tracker;