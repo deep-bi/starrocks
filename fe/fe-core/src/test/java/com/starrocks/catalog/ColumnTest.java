deep-bi/starrocks

commit 71548fa2bef4fe67a78bd1d7e4ac843bec2f0e19

[Refactor] Introduce ColumnId to support Column renaming (part1) (#45364)
## Why I'm doing:
We need a unique ID to identify the Column. This ID is used in all places that reference the Column. In this way, to change the attributes of the Column, such as name, we only need to change the attributes in the Column object. There is a uniqueId id the current Column, but it is only available in newly created tables. For compatibility reasons, we need to introduce another Id: ColumnID. The ColumnID of the historical table is the name of the column, because the name was previously immutable.

## What I'm doing:
Add ColumnId object.

Signed-off-by: gengjun-git <gengjun@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
@@ -82,13 +82,16 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
     @SerializedName(value = "name")
     private String name;
 
-    // physicalName is the column name used in the storage engine and will never change.
-    // The name saved in the storage engine remains unchanged after the logical column name is changed.
-    // By default, this value is null, which expresses the same as name (logical name).
-    // If the column name is changed, the value of name (logical name) will be updated to the new column name
-    // and the value of physicalName will be set to the old column name.
-    @SerializedName(value = "physicalName")
-    private String physicalName;
+    // For OLAP Table and its sub classes:
+    // When column is created, columnId is same to name.
+    // If the column name is changed, the value of name will be updated to the new column name,
+    // and the value of columnId remains unchanged.
+    //
+    // For other tables: columnId is same to name.
+    //
+    // All references to Column should use columnId instead of name.
+    @SerializedName(value = "columnId")
+    private ColumnId columnId;
 
     @SerializedName(value = "type")
     private Type type;
@@ -131,6 +134,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
 
     public Column() {
         this.name = "";
+        this.columnId = ColumnId.create(this.name);
         this.type = Type.NULL;
         this.isAggregationTypeImplicit = false;
         this.isKey = false;
@@ -179,7 +183,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
         if (this.name == null) {
             this.name = "";
         }
-
+        this.columnId = ColumnId.create(this.name);
         this.type = type;
         if (this.type == null) {
             this.type = Type.NULL;
@@ -210,6 +214,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
 
     public Column(Column column) {
         this.name = column.getName();
+        this.columnId = column.getColumnId();
         this.type = column.type;
         this.aggregationType = column.getAggregationType();
         this.isAggregationTypeImplicit = column.isAggregationTypeImplicit();
@@ -388,7 +393,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
 
     public TColumn toThrift() {
         TColumn tColumn = new TColumn();
-        tColumn.setColumn_name(this.getPhysicalName());
+        tColumn.setColumn_name(this.columnId.getId());
         tColumn.setIndex_len(this.getOlapColumnIndexSize());
         tColumn.setType_desc(this.type.toThrift());
         if (null != this.aggregationType) {
@@ -695,7 +700,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.name.toLowerCase(), this.type);
+        return Objects.hash(this.columnId.getId().toLowerCase(), this.type);
     }
 
     @Override
@@ -781,6 +786,10 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
             generatedColumnExpr = SqlParser.parseSqlToExpr(generatedColumnExprSerialized.expressionSql,
                     SqlModeHelper.MODE_DEFAULT);
         }
+
+        if (columnId == null) {
+            columnId = ColumnId.create(name);
+        }
     }
 
     @Override
@@ -790,19 +799,8 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
         }
     }
 
-    public String getPhysicalName() {
-        return physicalName != null ? physicalName : name;
-    }
-
-    public String getDirectPhysicalName() {
-        return physicalName != null ? physicalName : "";
-    }
-
-    public void renameColumn(String newName) {
-        if (physicalName == null) {
-            physicalName = name;
-        }
-        this.name = newName;
+    public ColumnId getColumnId() {
+        return columnId;
     }
 
     public void setUniqueId(int colUniqueId) {

fe/fe-core/src/main/java/com/starrocks/alter/SchemaChangeHandler.java
@@ -889,14 +889,14 @@ public class SchemaChangeHandler extends AlterHandler {
                     "Can not add column which already exists in base table: " + newColName);
         }
 
-        // check if the new column already exist in physical column.
-        // do not support adding new column which already exist in physical column.
-        Optional<Column> foundPhysicalColumn = olapTable.getBaseSchema().stream()
-                .filter(c -> c.getDirectPhysicalName().equalsIgnoreCase(newColName)).findFirst();
-        if (foundPhysicalColumn.isPresent()) {
+        // check if the new column already exist in column id.
+        // do not support adding new column which already exist in column id.
+        foundColumn = olapTable.getBaseSchema().stream()
+                .filter(c -> c.getColumnId().getId().equalsIgnoreCase(newColName)).findFirst();
+        if (foundColumn.isPresent()) {
             throw new DdlException(
-                    "Can not add column which already exists in physical column: " + newColName
-                            + ", you can remove " + foundPhysicalColumn.get() + " and try again.");
+                    "Can not add column which already exists in column id: " + newColName
+                            + ", you can remove " + foundColumn.get() + " and try again.");
         }
 
         /*

fe/fe-core/src/main/java/com/starrocks/analysis/SlotDescriptor.java
@@ -264,8 +264,8 @@ public class SlotDescriptor {
             type = type.isNull() ? ScalarType.BOOLEAN : type;
             tSlotDescriptor.setSlotType(type.toThrift());
             if (column != null) {
-                LOG.debug("column physical name:{}, column unique id:{}",
-                        column.getPhysicalName(), column.getUniqueId());
+                LOG.debug("column id:{}, column unique id:{}",
+                        column.getColumnId(), column.getUniqueId());
                 tSlotDescriptor.setCol_unique_id(column.getUniqueId());
             }
         }
@@ -273,7 +273,7 @@ public class SlotDescriptor {
         tSlotDescriptor.setByteOffset(-1);
         tSlotDescriptor.setNullIndicatorByte(-1);
         tSlotDescriptor.setNullIndicatorBit(nullIndicatorBit);
-        tSlotDescriptor.setColName(((column != null) ? column.getPhysicalName() : ""));
+        tSlotDescriptor.setColName(((column != null) ? column.getColumnId().getId() : ""));
         tSlotDescriptor.setSlotIdx(-1);
         tSlotDescriptor.setIsMaterialized(true);
         tSlotDescriptor.setIsOutputColumn(isOutputColumn);

fe/fe-core/src/main/java/com/starrocks/catalog/ColumnId.java
@@ -1,0 +1,79 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.catalog;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class ColumnId {
+    private final String id;
+
+    public ColumnId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static ColumnId create(String id) {
+        return new ColumnId(id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ColumnId columnId = (ColumnId) o;
+        return Objects.equals(id, columnId.id);
+    }
+
+    public boolean equalsIgnoreCase(ColumnId anotherId) {
+        if (this == anotherId) {
+            return true;
+        }
+
+        if (anotherId == null) {
+            return false;
+        }
+
+        return id != null ? id.equalsIgnoreCase(anotherId.id) : anotherId.id == null;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+    public static final Comparator<ColumnId> CASE_INSENSITIVE_ORDER =
+            new ColumnId.CaseInsensitiveComparator();
+
+    private static class CaseInsensitiveComparator implements Comparator<ColumnId> {
+        public int compare(ColumnId n1, ColumnId n2) {
+            return String.CASE_INSENSITIVE_ORDER.compare(n1.id, n2.id);
+        }
+    }
+}

fe/fe-core/src/main/java/com/starrocks/catalog/HiveTable.java
@@ -39,7 +39,6 @@ import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -286,7 +285,6 @@ public class HiveTable extends Table implements HiveMetaStoreTable {
 
     public void modifyTableSchema(String dbName, String tableName, HiveTable updatedTable) {
         ImmutableList.Builder<Column> fullSchemaTemp = ImmutableList.builder();
-        ImmutableMap.Builder<String, Column> nameToColumnTemp = ImmutableMap.builder();
         ImmutableList.Builder<String> dataColumnNamesTemp = ImmutableList.builder();
 
         updatedTable.nameToColumn.forEach((colName, column) -> {
@@ -297,7 +295,6 @@ public class HiveTable extends Table implements HiveMetaStoreTable {
         });
 
         fullSchemaTemp.addAll(updatedTable.fullSchema);
-        nameToColumnTemp.putAll(updatedTable.nameToColumn);
         dataColumnNamesTemp.addAll(updatedTable.dataColumnNames);
 
         Database db = GlobalStateMgr.getCurrentState().getDb(dbName);
@@ -312,7 +309,7 @@ public class HiveTable extends Table implements HiveMetaStoreTable {
             this.dataColumnNames.clear();
 
             this.fullSchema.addAll(fullSchemaTemp.build());
-            this.nameToColumn.putAll(nameToColumnTemp.build());
+            updateSchemaIndex();
             this.dataColumnNames.addAll(dataColumnNamesTemp.build());
 
             if (GlobalStateMgr.getCurrentState().isLeader()) {

fe/fe-core/src/main/java/com/starrocks/catalog/OlapTable.java
@@ -319,7 +319,12 @@ public class OlapTable extends Table {
         olapTable.id = this.id;
         olapTable.name = this.name;
         olapTable.fullSchema = Lists.newArrayList(this.fullSchema);
-        olapTable.nameToColumn = Maps.newHashMap(this.nameToColumn);
+        Map<String, Column> nameToColumn = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
+        nameToColumn.putAll(this.nameToColumn);
+        olapTable.nameToColumn = nameToColumn;
+        Map<ColumnId, Column> idToColumn = Maps.newTreeMap(ColumnId.CASE_INSENSITIVE_ORDER);
+        idToColumn.putAll(this.idToColumn);
+        olapTable.idToColumn = idToColumn;
         olapTable.state = this.state;
         olapTable.indexNameToId = Maps.newHashMap(this.indexNameToId);
         olapTable.indexIdToMeta = Maps.newHashMap(this.indexIdToMeta);
@@ -585,20 +590,21 @@ public class OlapTable extends Table {
     // the order of columns in fullSchema is meaningless
     public void rebuildFullSchema() {
         List<Column> newFullSchema = new CopyOnWriteArrayList<>();
-        nameToColumn = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
+        Set<String> addedColumnNames = Sets.newTreeSet(String.CASE_INSENSITIVE_ORDER);
         for (Column baseColumn : indexIdToMeta.get(baseIndexId).getSchema()) {
             newFullSchema.add(baseColumn);
-            nameToColumn.put(baseColumn.getName(), baseColumn);
+            addedColumnNames.add(baseColumn.getName());
         }
         for (MaterializedIndexMeta indexMeta : indexIdToMeta.values()) {
             for (Column column : indexMeta.getSchema()) {
-                if (!nameToColumn.containsKey(column.getName())) {
+                if (!addedColumnNames.contains(column.getName())) {
                     newFullSchema.add(column);
-                    nameToColumn.put(column.getName(), column);
+                    addedColumnNames.add(column.getName());
                 }
             }
         }
         fullSchema = newFullSchema;
+        updateSchemaIndex();
         // update max column unique id
         int maxColUniqueId = getMaxColUniqueId();
         for (Column column : fullSchema) {
@@ -671,6 +677,11 @@ public class OlapTable extends Table {
         return Lists.newArrayList();
     }
 
+    @Override
+    public Column getColumn(ColumnId id) {
+        return idToColumn.get(id);
+    }
+
     // this is only for schema change.
     public void renameIndexForSchemaChange(String name, String newName) {
         long idxId = indexNameToId.remove(name);
@@ -682,6 +693,13 @@ public class OlapTable extends Table {
         for (Column column : columns) {
             column.setName(Column.removeNamePrefix(column.getName()));
         }
+    }
+
+    public void renameColumn(String oldName, String newName) {
+        Column column = this.nameToColumn.remove(oldName);
+        Preconditions.checkState(column != null, "column of name: " + oldName + " does not exist");
+        column.setName(newName);
+        this.nameToColumn.put(newName, column);
     }
 
     public Status resetIdsForRestore(GlobalStateMgr globalStateMgr, Database db, int restoreReplicationNum,
@@ -2178,6 +2196,24 @@ public class OlapTable extends Table {
     @Override
     public List<Column> getBaseSchema() {
         return getSchemaByIndexId(baseIndexId);
+    }
+
+    @Override
+    protected void updateSchemaIndex() {
+        Map<String, Column> newNameToColumn = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
+        Map<ColumnId, Column> newIdToColumn = Maps.newTreeMap(ColumnId.CASE_INSENSITIVE_ORDER);
+        for (Column column : this.fullSchema) {
+            newNameToColumn.put(column.getName(), column);
+            // For OlapTable: fullSchema contains columns from baseIndex
+            // and columns from shadow indexes (when doing schema changes).
+            // To avoid base columns being replaced by shadow columns,
+            // put the columns with the same ColumnId once.
+            if (!newIdToColumn.containsKey(column.getColumnId())) {
+                newIdToColumn.put(column.getColumnId(), column);
+            }
+        }
+        this.nameToColumn = newNameToColumn;
+        this.idToColumn = newIdToColumn;
     }
 
     public List<Column> getBaseSchemaWithoutGeneratedColumn() {

commit 147ba3239558608d911164ceaea8c42aee4a2218

[BugFix] Fix ColumnId empty bug when upgrading (#45594)
## Why I'm doing:
Introduced by #45364

The default value of columnId is set to empty in `Column()`, which will cause the logic in `gsonPostProcess()` not to be executed during the upgrade.

Signed-off-by: gengjun-git <gengjun@starrocks.com>

fe/fe-core/src/main/java/com/starrocks/catalog/Column.java
@@ -132,9 +132,9 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
     private GsonUtils.ExpressionSerializedObject generatedColumnExprSerialized;
     private Expr generatedColumnExpr;
 
+    // Only for persist
     public Column() {
         this.name = "";
-        this.columnId = ColumnId.create(this.name);
         this.type = Type.NULL;
         this.isAggregationTypeImplicit = false;
         this.isKey = false;
@@ -788,7 +788,7 @@ public class Column implements Writable, GsonPreProcessable, GsonPostProcessable
                     SqlModeHelper.MODE_DEFAULT);
         }
 
-        if (columnId == null) {
+        if (columnId == null || Strings.isNullOrEmpty(columnId.getId())) {
             columnId = ColumnId.create(name);
         }
     }

fe/fe-core/src/test/java/com/starrocks/analysis/AccessTestUtil.java
@@ -78,7 +78,7 @@ public class AccessTestUtil {
         RandomDistributionInfo distributionInfo = new RandomDistributionInfo(10);
         Partition partition = new Partition(20000L, "testTbl", baseIndex, distributionInfo);
         List<Column> baseSchema = new LinkedList<Column>();
-        Column column = new Column();
+        Column column = new Column("k1", Type.INT);
         baseSchema.add(column);
         OlapTable table = new OlapTable(30000, "testTbl", baseSchema,
                 KeysType.AGG_KEYS, new SinglePartitionInfo(), distributionInfo, null);

fe/fe-core/src/test/java/com/starrocks/catalog/ColumnTest.java
@@ -39,6 +39,7 @@ import com.starrocks.analysis.StringLiteral;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.FeConstants;
 import com.starrocks.common.jmockit.Deencapsulation;
+import com.starrocks.persist.gson.GsonUtils;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.sql.ast.ColumnDef;
 import com.starrocks.sql.ast.ColumnDef.DefaultValueDef;
@@ -352,5 +353,12 @@ public class ColumnTest {
 
         Assert.assertEquals(f0.getUniqueId(), 1);
 
+    }
+
+    @Test
+    public void testColumnDeserialization() {
+        String str = "{\"name\": \"test\"}";
+        Column column = GsonUtils.GSON.fromJson(str, Column.class);
+        Assert.assertEquals("test", column.getColumnId().getId());
     }
 }

fe/fe-core/src/test/java/com/starrocks/clone/TabletSchedulerTest.java
@@ -30,6 +30,7 @@ import com.starrocks.catalog.SchemaInfo;
 import com.starrocks.catalog.Table;
 import com.starrocks.catalog.TabletInvertedIndex;
 import com.starrocks.catalog.TabletMeta;
+import com.starrocks.catalog.Type;
 import com.starrocks.common.Config;
 import com.starrocks.common.Pair;
 import com.starrocks.common.jmockit.Deencapsulation;
@@ -395,7 +396,7 @@ public class TabletSchedulerTest {
                 .setShortKeyColumnCount((short) 1)
                 .setSchemaHash(-1)
                 .setStorageType(TStorageType.COLUMN)
-                .addColumn(new Column())
+                .addColumn(new Column("k1", Type.INT))
                 .build().toTabletSchema();
 
         CreateReplicaTask createReplicaTask = CreateReplicaTask.newBuilder()

fe/fe-core/src/test/java/com/starrocks/connector/odps/OdpsTableTest.java
@@ -16,6 +16,7 @@ package com.starrocks.connector.odps;
 
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.OdpsTable;
+import com.starrocks.catalog.Type;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -63,7 +64,7 @@ public class OdpsTableTest {
     public void testDataColumnNames() {
         OdpsTable odpsTable = mock(OdpsTable.class);
         List<Column> dataColumns = new ArrayList<>();
-        Column column = new Column();
+        Column column = new Column("k1", Type.INT);
         column.setName("name");
         dataColumns.add(column);
         when(odpsTable.getDataColumnNames()).thenReturn(
@@ -75,7 +76,7 @@ public class OdpsTableTest {
     public void testPartitionColumns() {
         OdpsTable odpsTable = mock(OdpsTable.class);
         List<Column> partitionColumns = new ArrayList<>();
-        Column column = new Column();
+        Column column = new Column("k1", Type.INT);
         column.setName("name");
         partitionColumns.add(column);
         when(odpsTable.getPartitionColumns()).thenReturn(partitionColumns);
@@ -86,7 +87,7 @@ public class OdpsTableTest {
     public void testPartitionColumnNames() {
         OdpsTable odpsTable = mock(OdpsTable.class);
         List<Column> partitionColumns = new ArrayList<>();
-        Column column = new Column();
+        Column column = new Column("k1", Type.INT);
         column.setName("name");
         partitionColumns.add(column);
         when(odpsTable.getPartitionColumnNames()).thenReturn(

fe/fe-core/src/test/java/com/starrocks/persist/gson/GsonSerializationTest.java
@@ -28,6 +28,7 @@ import com.google.common.collect.Table;
 import com.google.gson.annotations.SerializedName;
 import com.starrocks.catalog.Column;
 import com.starrocks.catalog.MaterializedIndexMeta;
+import com.starrocks.catalog.Type;
 import com.starrocks.common.io.FastByteArrayOutputStream;
 import com.starrocks.common.io.Text;
 import com.starrocks.common.io.Writable;
@@ -493,7 +494,7 @@ public class GsonSerializationTest {
 
     @Test
     public void testMaterializedIndexMetaGsonProcess() throws Exception {
-        MaterializedIndexMeta indexMeta = new MaterializedIndexMeta(1, Lists.newArrayList(new Column()), 1, 1,
+        MaterializedIndexMeta indexMeta = new MaterializedIndexMeta(1, Lists.newArrayList(new Column("k1", Type.INT)), 1, 1,
                 (short) 1, COLUMN, PRIMARY_KEYS, null);
         FastByteArrayOutputStream byteArrayOutputStream = new FastByteArrayOutputStream();
         try (DataOutputStream out = new DataOutputStream(byteArrayOutputStream)) {

fe/fe-core/src/test/java/com/starrocks/sql/analyzer/MaterializedViewAnalyzerTest.java
@@ -25,6 +25,7 @@ import com.starrocks.catalog.PaimonTable;
 import com.starrocks.catalog.PrimitiveType;
 import com.starrocks.catalog.ScalarType;
 import com.starrocks.catalog.Table;
+import com.starrocks.catalog.Type;
 import com.starrocks.common.DdlException;
 import com.starrocks.common.Pair;
 import com.starrocks.qe.ShowExecutor;
@@ -367,7 +368,7 @@ public class MaterializedViewAnalyzerTest {
     private void checkQueryOutputIndices(List<Integer> inputs, String expect, boolean isChanged) {
         List<Pair<Column, Integer>> mvColumnPairs = Lists.newArrayList();
         for (Integer i : inputs) {
-            mvColumnPairs.add(Pair.create(new Column(), i));
+            mvColumnPairs.add(Pair.create(new Column("k1", Type.INT), i));
         }
         List<Integer> queryOutputIndices = MaterializedViewAnalyzer.getQueryOutputIndices(mvColumnPairs);
         Assert.assertTrue(queryOutputIndices.size() == mvColumnPairs.size());

fe/fe-core/src/test/java/com/starrocks/sql/optimizer/OptimizerTaskTest.java
@@ -1188,7 +1188,7 @@ public class OptimizerTaskTest {
         List<ColumnRefOperator> scanColumns = Lists.newArrayList(column1);
 
         Map<ColumnRefOperator, Column> scanColumnMap = Maps.newHashMap();
-        scanColumnMap.put(column1, new Column());
+        scanColumnMap.put(column1, new Column("k1", Type.INT));
 
         Map<ColumnRefOperator, CallOperator> map = Maps.newHashMap();
         LogicalAggregationOperator aggregationOperator =
@@ -1427,7 +1427,7 @@ public class OptimizerTaskTest {
 
         List<ColumnRefOperator> scanColumns = Lists.newArrayList(column1);
         Map<ColumnRefOperator, Column> scanColumnMap = Maps.newHashMap();
-        scanColumnMap.put(column1, new Column());
+        scanColumnMap.put(column1, new Column("k1", Type.INT));
 
         Map<ColumnRefOperator, ScalarOperator> projectColumnMap = Maps.newHashMap();
         projectColumnMap.put(column1, column1);
@@ -1548,7 +1548,7 @@ public class OptimizerTaskTest {
         List<ColumnRefOperator> scanColumns = Lists.newArrayList(column1, column2);
 
         Map<ColumnRefOperator, Column> scanColumnMap = Maps.newHashMap();
-        scanColumnMap.put(column1, new Column());
+        scanColumnMap.put(column1, new Column("k1", Type.INT));
 
         Map<ColumnRefOperator, ScalarOperator> projectColumnMap1 = Maps.newHashMap();
         projectColumnMap1.put(column3, column1);

fe/fe-core/src/test/java/com/starrocks/sql/optimizer/rule/mv/MaterializedViewRuleTest.java
@@ -91,7 +91,7 @@ public class MaterializedViewRuleTest extends PlanTestBase {
         ColumnRefFactory tmpRefFactory = new ColumnRefFactory();
         ColumnRefOperator queryColumnRef = tmpRefFactory.create("count", Type.INT, false);
         ColumnRefOperator mvColumnRef = tmpRefFactory.create("count", Type.INT, false);
-        Column mvColumn = new Column();
+        Column mvColumn = new Column("k1", Type.INT);
         List<ScalarOperator> arguments = Lists.newArrayList();
         arguments.add(queryColumnRef);
         CallOperator aggCall = new CallOperator(FunctionSet.COUNT, Type.BIGINT, arguments);