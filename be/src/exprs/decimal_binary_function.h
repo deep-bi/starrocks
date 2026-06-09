7a7e21bcd2d09999809f56df722cbe1a25d33e55
ThomasPszowski <162191217+ThomasPszowski@users.noreply.github.com>
2026-06-09 16:59:55 +0200

    Update decimal_binary_function.h

--- be/src/exprs/decimal_binary_function.h
+++ be/src/exprs/decimal_binary_function.h
@@ -70,10 +70,10 @@ struct DecimalBinaryFunction {
                 if (overflow) {
                     if constexpr (is_div_op<Op>) {
                         using_int256_division = true;
-                        //signed_multiply_128x128_to_256(lhs_datum, scale_factor, lhs_scaled_int256_low, lhs_scaled_int256_high);
+                        signed_multiply_128x128_to_256(lhs_datum, scale_factor, lhs_scaled_int256_low, lhs_scaled_int256_high);
                         overflow = false;
                     }
-                    else if constexpr (check_overflow<overflow_mode>)
+                    else if constexpr (check_overflow<overflow_mode>) {
                         if constexpr (error_if_overflow<overflow_mode>) {
                             throw std::overflow_error(strings::Substitute(
                                     "The '$0' operation involving decimal values overflows", get_op_name<Op>()));
