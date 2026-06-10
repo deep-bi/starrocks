97198417c76ba75f6601bffbda45233c33c563b3
ThomasPszowski <162191217+ThomasPszowski@users.noreply.github.com>
2026-06-10 14:36:35 +0200

    test

--- be/src/types/int_256.h
+++ be/src/types/int_256.h
@@ -165,7 +165,7 @@ static inline void signed_multiply_128x128_to_256(int128_t x,
 static inline int128_t signed_div_256_by_128_to_128(
     int128_t high,
     int128_t low,
-    int128_t divisor) {
+    int128_t divisor, bool& overflow) {
     // static constexpr int128_t INT128_MAX = (int128_t)((uint128_t(1) << 127) - 1);
     // static constexpr int128_t INT128_MIN = -INT128_MAX - 1;
     
